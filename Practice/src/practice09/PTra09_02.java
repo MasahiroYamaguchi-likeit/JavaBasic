db33694c57be18cf71bfff7735eac18030b04c3d
yamaguchi masahiro <kandai.y.m@gmail.com>
2018-04-04 18:37:36 +0900

    PTra 9　完了

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -1,10 +1,14 @@
 package practice09;
+
+import practice09.util.Util;
+
 /*
  * PTra09_02.java
  *   作成	LIKEIT	2017
  *------------------------------------------------------------
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
+
 
 public class PTra09_02 {
 	public static void main(String[] args) {
@@ -13,8 +17,12 @@ public class PTra09_02 {
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
 
+		String[] list = Util.shuffleArray(array);
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
 
+		for( int i = 0; i<array.length; i++ ) {
+			System.out.println(array[i]);
+		}
 	}
 }
